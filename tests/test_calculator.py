ef4b2925e72d469501f67d573b729a4cf85f8aeb
amycruiz <acr@njit.edu>
2024-09-28 13:19:21 -0400

    revamped the files in the calculator and tests folders in part3 branch in order to create an improved calculator for part 3 of HW3. changed the .pylintrc file to allow each pytest to be passed. passed each pytest successfully.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,17 +1,18 @@
+"Testing calculator"
 from calculator import Calculator
 
 def test_addition():
-    '''Test addition'''
+    "Testing addition"
     assert Calculator.add(2,2) == 4
 
 def test_subtraction():
-    '''Test subtraction'''
+    "Testing subtraction"
     assert Calculator.subtract(2,2) == 0
 
-def test_multiply():
-    '''Test multiplication'''
-    assert Calculator.multiply(2,2) == 4
-
 def test_divide():
-    '''Test division'''
+    "Testing division"
     assert Calculator.divide(2,2) == 1
+
+def test_multiply():
+    "Testing multiplication"
+    assert Calculator.multiply(2,2) == 4
