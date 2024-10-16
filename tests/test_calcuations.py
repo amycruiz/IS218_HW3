7c3214c419785dc185371386e6bf65a3755ee230
amycruiz <acr@njit.edu>
2024-10-15 20:53:55 -0400

    modified files in order for all pytest tests to be passed.

--- tests/test_calcuations.py
+++ tests/test_calcuations.py
@@ -1,5 +1,7 @@
-# Testing Calculations
 '''
+Testing Calculations
+'''
+
 from decimal import Decimal
 import pytest
 from calculator.calculation import Calculation
@@ -17,14 +19,14 @@ def test_add_calculation(setup_calculations):
     "Testing the adding calcuation"
     calc = Calculation(Decimal('2'), Decimal('2'), add)
     Calculations.add_calculation(calc)
-    assert Calculations.get_latest() == calc, "Failed to add the
-    calculation to the history"
+    assert Calculations.get_latest() == calc, '''Failed to add the
+    calculation to the history'''
 
 def test_get_history(setup_calculations):
     "Testing the history of calculations"
     history = Calculations.get_history()
-    assert len(history) == 2, "History does not contain
-    the expected number of calculations"
+    assert len(history) == 2, '''History does not contain
+    the expected number of calculations'''
 
 def test_clear_history(setup_calculations):
     "Testing the clearing of history"
@@ -34,21 +36,20 @@ def test_clear_history(setup_calculations):
 def test_get_latest(setup_calculations):
     "Testing the latest calculation"
     latest = Calculations.get_latest()
-    assert latest.a == Decimal('20') and latest.b == Decimal('3'), "Did not get
-    the correct latest calculation"
+    assert latest.a == Decimal('20') and latest.b == Decimal('3'), '''Did not get
+    the correct latest calculation'''
 
 def test_find_by_operation(setup_calculations):
     "Testing the add and subtract operations"
     add_operations = Calculations.find_by_operation("add")
-    assert len(add_operations) == 1, "Did not find the correct number
-    of calculations with add operation"
+    assert len(add_operations) == 1, '''Did not find the correct number
+    of calculations with add operation'''
     subtract_operations = Calculations.find_by_operation("subtract")
-    assert len(subtract_operations) == 1, "Did not find the correct number
-    of calculations with subtract operation"
+    assert len(subtract_operations) == 1, '''Did not find the correct number
+    of calculations with subtract operation'''
 
 def test_get_latest_with_empty_history():
     "Testing the empty history"
     Calculations.clear_history()
-    assert Calculations.get_latest() is None, "Expected None for latest
-    calculation with empty history"
-'''
+    assert Calculations.get_latest() is None, '''Expected None for latest
+    calculation with empty history'''
